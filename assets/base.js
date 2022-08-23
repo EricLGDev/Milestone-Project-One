1ded39479af261a210b09fdf772691cafde0769b
Eric <ericlg.link@gmail.com>
2022-08-23 03:02:15 -0700

    Created a file for the base game js

--- assets/base.js
+++ assets/base.js
@@ -3,15 +3,13 @@ var canvas = document.getElementById("ptbreak"),
     gems = [],
     moves = []
 let gemList = [];
-
+//menu buttons
 let startBtn = document.getElementById("startBtn");
 let timeBtn = document.getElementById('timeBtn');
 let resetBtn = document.getElementById('resetBtn');
-
 let pressStart = document.getElementById('pressStart');
 let volumeImg = document.getElementById('volumeImg');
-
-
+//in-game stats
 let moveCount = 0;
 let timeCount = 0;
 let score = 0;
---
+++ assets/game.js
@@ -0,0 +1,9 @@
+//initialize the game
+
+//create the gem objects
+
+//define gem colors
+
+//set a way to clear gems once matched
+
+//game over requirement
