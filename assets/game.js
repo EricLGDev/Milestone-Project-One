a678d2c78167d8d37a15a220c60c919f7055bd31
Eric <ericlg.link@gmail.com>
2022-08-23 18:24:46 -0700

    Defined draw

--- assets/game.js
+++ assets/game.js
@@ -29,6 +29,25 @@ function initialize() {
 
     draw();
 }
+
+function draw() {
+
+    // Clear the canvas
+    c.clearRect(0, 0, 600, 700);
+
+    for (let x = 0; x < 10; x++) {
+        for (let y = 0; y < 10; y++) {
+            c.drawImage(gemList[gems[x][y].color], x * 60, gems[x][y].getY(), 50, 50);
+        }
+    }
+
+    c.font = 'bold 20px sans-serif';
+    c.textAlign = 'center';
+    c.fillText("Moves Left : " + moveCount, 150, 50);
+    c.fillText("Score: " + score, 400, 50);
+
+}
+
 //create the gem objects
 
 //define gem colors
